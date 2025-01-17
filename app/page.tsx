8ccc26a5e50f5ac83d2b4c83620bbc1c6344454a
Arjun Mohan <arjunpotter17@gmail.com>
2025-01-17 18:37:38 +0530

    POC

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,86 @@
-import Image from "next/image";
+"use client";
+import { FetchStore, get, open } from "zarrita";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const fetchZarr = async () => {
+    try {
+      const sstStore = new FetchStore("http://localhost:8080/output.zarr/sst");
+      const sstArray = await open(sstStore, { kind: "array" });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      //this is equivalent to the dimensions in netcdf
+      const [timeLen, depthLen, latLen, lonLen] = sstArray.shape;
+
+      //get the chunks that data is comprssed into
+      const chunks = sstArray.chunks;
+
+      const [, depthChunks, latChunks, lonChunks] = chunks;
+
+      const minLat = 142;
+      const maxLat = latLen - 142;
+      const minLon = 1;
+      const maxLon = lonLen - 1;
+
+      //check if time is within bounds to push to db, not used for now
+      const timeStore = new FetchStore(
+        "http://localhost:8080/output.zarr/time"
+      );
+      const timeArray = await open(timeStore, { kind: "array" });
+      const timeData = await get(timeArray);
+      const time = timeData.data as Float32Array;
+      console.log("this is time", time[0] * 24 * 60 * 60 * 1000);
+      const latestDate = new Date(time[0] * 24 * 60 * 60 * 1000);
+      console.log("Latest date:", latestDate.toISOString());
+
+
+      //this part is sketchy and needs work and testing
+      const validData = [];
+      for (let i = minLat; i < maxLat; i++) {
+        for (let j = minLon; j < maxLon; j++) {
+          const lat_chunk_idx = i / 180;
+          const long_chunk_idx = j / 720;
+
+          const chunk = await sstArray.getChunk([
+            0,
+            0,
+            lat_chunk_idx,
+            long_chunk_idx,
+          ]);
+
+          const time_offset = timeLen - 1;
+
+          const depth_offset = depthLen - 1;
+
+          const lat_offset = i % latChunks;
+
+          const lon_offset = j % lonChunks;
+
+          const index =
+            time_offset * depthChunks * latChunks * lonChunks +
+            depth_offset * latChunks * lonChunks +
+            lat_offset * lonChunks +
+            lon_offset;
+
+          const data = chunk.data as Int16Array;
+          const value = data[index];
+
+          console.log("this is value", value);
+          validData.push(value);
+        }
+      }
+      const validChunks = await sstArray.getChunk([0, 0, 0, 0]);
+      console.log("this is validChunks", validChunks);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const dataForChunk = validChunks.data as any;
+      console.log("this is data for chunk", dataForChunk);
+      console.log("this is data", dataForChunk[102290]);
+    } catch (error) {
+      console.error("Error fetching Zarr data:", error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={fetchZarr}>Fuck with Zarr</button>
     </div>
   );
 }
