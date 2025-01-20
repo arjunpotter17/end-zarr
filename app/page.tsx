948c22e6016035b2ffdebe2ea74781845fe0a987
Arjun Mohan <arjunpotter17@gmail.com>
2025-01-20 18:02:01 +0530

    feat: converted compute

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FetchStore, get, open } from "zarrita";
+import { FetchStore, open } from "zarrita";
 
 export default function Home() {
   const fetchZarr = async () => {
@@ -7,80 +7,76 @@ export default function Home() {
       const sstStore = new FetchStore("http://localhost:8080/output.zarr/sst");
       const sstArray = await open(sstStore, { kind: "array" });
 
-      //this is equivalent to the dimensions in netcdf
+      //this is equivalent to the dimensions in netcdf (but notice that a depth layer has been added here which was not present in .nc)
       const [timeLen, depthLen, latLen, lonLen] = sstArray.shape;
 
-      //get the chunks that data is comprssed into
+      //get the chunks that data is compressed into
       const chunks = sstArray.chunks;
 
-      const [, depthChunks, latChunks, lonChunks] = chunks;
+      const [, , latChunkSize, lonChunkSize] = chunks;
 
+      // I copied this over from map-builder but we don't need this since data is compressed into chunks and we just map over the chunks
       const minLat = 142;
       const maxLat = latLen - 142;
       const minLon = 1;
       const maxLon = lonLen - 1;
 
-      //check if time is within bounds to push to db, not used for now
-      const timeStore = new FetchStore(
-        "http://localhost:8080/output.zarr/time"
-      );
-      const timeArray = await open(timeStore, { kind: "array" });
-      const timeData = await get(timeArray);
-      const time = timeData.data as Float32Array;
-      console.log("this is time", time[0] * 24 * 60 * 60 * 1000);
-      const latestDate = new Date(time[0] * 24 * 60 * 60 * 1000);
-      console.log("Latest date:", latestDate.toISOString());
 
 
-      //this part is sketchy and needs work and testing
+      // reference date calucation, I'm not sure why it's done in mapbuilder 
+
+      // const timeStore = new FetchStore(
+      //   "http://localhost:8080/output.zarr/time"
+      // );
+      // const timeArray = await open(timeStore, { kind: "array" });
+      // const timeData = await get(timeArray);
+      // const time = timeData.data as Float32Array;
+      // console.log("this is time", time[0] * 24 * 60 * 60 * 1000);
+      // const latestDate = new Date(time[0] * 24 * 60 * 60 * 1000);
+      // console.log("Latest date:", latestDate.toISOString());
+
+      //end of ref date calc
+
+
+
+
       const validData = [];
-      for (let i = minLat; i < maxLat; i++) {
-        for (let j = minLon; j < maxLon; j++) {
-          const lat_chunk_idx = i / 180;
-          const long_chunk_idx = j / 720;
+      //map through each chunk, then inside each chunk map through all data, add data to validData array if meets conditions
+      for (let latChunk = 0; latChunk < latLen / latChunkSize; latChunk++) {
+        for (let lonChunk = 0; lonChunk < lonLen / lonChunkSize; lonChunk++) {
 
-          const chunk = await sstArray.getChunk([
-            0,
-            0,
-            lat_chunk_idx,
-            long_chunk_idx,
-          ]);
+          console.log('working on chunk', latChunk, lonChunk)
 
-          const time_offset = timeLen - 1;
-
-          const depth_offset = depthLen - 1;
-
-          const lat_offset = i % latChunks;
-
-          const lon_offset = j % lonChunks;
-
-          const index =
-            time_offset * depthChunks * latChunks * lonChunks +
-            depth_offset * latChunks * lonChunks +
-            lat_offset * lonChunks +
-            lon_offset;
-
+          const chunk = await sstArray.getChunk([timeLen - 1, depthLen - 1, latChunk, lonChunk]);
           const data = chunk.data as Int16Array;
-          const value = data[index];
-
-          console.log("this is value", value);
-          validData.push(value);
+          for (let i = 0; i < data.length; i++) {
+            const value = data[i];
+            if (value !== -999 && value > -3 && value < 45) {
+              validData.push(value);
+            }
+          }
         }
       }
-      const validChunks = await sstArray.getChunk([0, 0, 0, 0]);
-      console.log("this is validChunks", validChunks);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const dataForChunk = validChunks.data as any;
-      console.log("this is data for chunk", dataForChunk);
-      console.log("this is data", dataForChunk[102290]);
+
+      //consolidate values
+      if(validData.length > 0) {
+        const sum = validData.reduce((sum, index) => sum + index, 0);
+        const meanValue = sum / validData.length;
+        console.log('this ismean', meanValue);
+        return meanValue;
+      } else {
+        console.log('error: no valid data')
+      }
     } catch (error) {
       console.error("Error fetching Zarr data:", error);
+      return null;
     }
   };
 
   return (
     <div>
       <button onClick={fetchZarr}>Fuck with Zarr</button>
+      
     </div>
   );
 }
